96234125f157ae18fdefe45e3bd502e7a57ea992
CoochiecooBerrauslu <113683902+CoochiecooBerrauslu@users.noreply.github.com>
2023-02-25 21:42:34 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
         double pi = 3.14;
         double circumference = 2 * pi * radius;
         double area = (circumference * radius) / 2;
-        // another way of finding are is that Math.pow(radius,2)*pi or
+        // another way of finding is that Math.pow(radius,2)*pi or
         // radius * radius * pi
         System.out.println("Area of the circular region is : " + area +
                 " " + "Circumference of the circular region is : " + circumference);
